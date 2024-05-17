48c0a8be412b48791064b179f5104dd35df4bc11
wedyzza <ggwp.ezpezlmon@mail.ru>
2024-05-18 00:21:55 +0500

    Initial commit

---
+++ database/migrations/2024_05_17_190254_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->nullable(false)->constrained()->cascadeOnDelete();
+            $table->string('name')->nullable(false);
+            $table->string('description')->nullable(false);
+            $table->integer('price')->nullable(false);
+            $table->string('avatar')->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
